82042791a31aa1d7a78034ea8b10b6941e8ae22e
rebeccavl1 <rebeccavanlangelaan@yahoo.com>
2015-07-30 15:29:03 +0200

    introducing

---
+++ remarema-master/src/remarema/master/Master.java
@@ -0,0 +1,23 @@
+package remarema.master;
+
+import remarema.client.api.*;
+
+public class Master implements MasterInterface{
+
+	public Master() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public NodeConfiguration getConfigurationForNode(String nodeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean canIhazSupernodePleaz(NodeConfiguration nodeConfiguration) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
